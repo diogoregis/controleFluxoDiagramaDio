69ed1ac3b6390b25d0f0a81571bfb82a3839e63c
Diogo Regis <diogo.regis.silva@gmail.com>
2024-07-24 17:44:31 -0300

    criado class, interfece e implementadas

---
+++ src/repository/AparelhoTelefonico.java
@@ -0,0 +1,7 @@
+package repository;
+
+public interface AparelhoTelefonico {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
---
+++ src/repository/ReprodutorMusical.java
@@ -0,0 +1,7 @@
+package repository;
+
+public interface ReprodutorMusical {
+    void tocar();
+    void pausar();
+    void selecionarMusica(String musica);
+}
